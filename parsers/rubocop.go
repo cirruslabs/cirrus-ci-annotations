809b32cab92c39c56b1f05a1c0ee892d25e68271
Nikolay Edigaryev <edigaryev@gmail.com>
2020-07-08 18:03:07 +0300

    RuboCop support (#13)
    * RuboCop support (#11)
    
    * README.md: mention RSpec and RuboCop support

---
+++ parsers/rubocop.go
@@ -0,0 +1,97 @@
+package parsers
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/cirruslabs/cirrus-ci-annotations/model"
+	"io/ioutil"
+)
+
+// rubocopSeverityMapping translates RuboCop's severity constants[1] into GitHub
+// annotation levels using RuboCop's SimpleTextFormatter colors[2] as a hint.
+// [1]: https://www.rubydoc.info/gems/rubocop/RuboCop/Cop/Severity
+// [2]: https://www.rubydoc.info/gems/rubocop/RuboCop/Formatter/SimpleTextFormatter#COLOR_FOR_SEVERITY-constant
+var rubocopSeverityMapping = map[string]string{
+	"refactor":   "notice",
+	"convention": "notice",
+	"warning":    "warning",
+	"error":      "failure",
+	"fatal":      "failure",
+}
+
+type rubocopLocation struct {
+	Line   int64
+	Column int64
+}
+
+type rubocopOffense struct {
+	Severity  string
+	Message   string
+	Corrected bool
+	Location  rubocopLocation
+}
+
+type rubocopFile struct {
+	Path     string
+	Offenses []rubocopOffense
+}
+
+type rubocopReport struct {
+	Files []rubocopFile
+}
+
+// ParseRuboCopAnnotations parses RuboCop's JSON output into annotations according to the documentation
+// ranged from 0.53.0[1] to 0.87.1[2].
+// [1]: https://github.com/rubocop-hq/rubocop/blob/v0.53.0/manual/formatters.md#json-formatter
+// [2]: https://github.com/rubocop-hq/rubocop/blob/v0.87.1/docs/modules/ROOT/pages/formatters.adoc#json-formatter
+func ParseRuboCopAnnotations(path string) (error, []model.Annotation) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err, nil
+	}
+
+	var report rubocopReport
+	err = json.Unmarshal(data, &report)
+	if err != nil {
+		return err, nil
+	}
+
+	result := make([]model.Annotation, 0)
+
+	for _, file := range report.Files {
+		for offenseNumber, offense := range file.Offenses {
+			// Map RuboCop's severity to GitHub's annotation level,
+			// skipping unknown severities.
+			level, found := rubocopSeverityMapping[offense.Severity]
+			if !found {
+				continue
+			}
+
+			// No matter the severity, corrected offenses get demoted to "notice" level
+			if offense.Corrected {
+				level = "notice"
+			}
+
+			// RuboCop does not provide a FQN, so we craft one ourselves
+			fqn := fmt.Sprintf("%s-%d", file.Path, offenseNumber)
+
+			var parsedAnnotation = model.Annotation{
+				Type:               model.LintResultAnnotationType,
+				Level:              level,
+				Message:            offense.Message,
+				FullyQualifiedName: fqn,
+				Location: &model.FileLocation{
+					Path:        file.Path,
+					StartLine:   offense.Location.Line,
+					EndLine:     offense.Location.Line,
+					StartColumn: offense.Location.Column,
+					EndColumn:   offense.Location.Column,
+				},
+			}
+
+			result = append(result, parsedAnnotation)
+		}
+	}
+
+	return nil, result
+}
---
+++ parsers/rubocop_test.go
@@ -0,0 +1,51 @@
+package parsers
+
+import (
+	"github.com/cirruslabs/cirrus-ci-annotations/model"
+	"github.com/go-test/deep"
+	"path/filepath"
+	"testing"
+)
+
+// Test_RuboCop_DocsExample ensures that JSON example from the documentation[1] is parsed properly.
+// [1]: https://github.com/rubocop-hq/rubocop/blob/master/docs/modules/ROOT/pages/formatters.adoc#json-formatter
+func Test_RuboCop_DocsExample(t *testing.T) {
+	var expectedAnnotations = []model.Annotation{
+		{
+			Type:               model.LintResultAnnotationType,
+			Level:              "notice",
+			Message:            "Line is too long. [81/80]",
+			FullyQualifiedName: "lib/bar.rb-0",
+			Location: &model.FileLocation{
+				Path:        "lib/bar.rb",
+				StartLine:   546,
+				EndLine:     546,
+				StartColumn: 80,
+				EndColumn:   80,
+			},
+		},
+		{
+			Type:               model.LintResultAnnotationType,
+			Level:              "warning",
+			Message:            "Unreachable code detected.",
+			FullyQualifiedName: "lib/bar.rb-1",
+			Location: &model.FileLocation{
+				Path:        "lib/bar.rb",
+				StartLine:   15,
+				EndLine:     15,
+				StartColumn: 9,
+				EndColumn:   9,
+			},
+		},
+	}
+
+	err, gotAnnotations := ParseRuboCopAnnotations(filepath.Join("..", "testdata", "json", "rubocop.json"))
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if diff := deep.Equal(expectedAnnotations, gotAnnotations); diff != nil {
+		t.Error(diff)
+	}
+}
